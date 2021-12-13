fedbf1d12efe3b11ba238c992efb79891166b420
ericwafula <wathomeeric@gmail.com>
2021-12-13 21:42:04 +0300

    updated es5 global scope and switched to es6 local scope syntax

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,16 @@
-$(Document).ready(function(){
-    function akanNameCalculator(day, month, year, gender) {
-        var cc, yy, dd, mm;
+$(Document).ready(() => {
+    let akanNameCalculator = (day, month, year, gender) => {
+        let cc, yy, dd, mm;
     
-        var male = ["Kwasi", " Kwadwo", " Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-        var female = ["Akosua", " Adwoa", " Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+        let male = ["Kwasi", " Kwadwo", " Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+        let female = ["Akosua", " Adwoa", " Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     
         cc = parseInt(year.slice(0, 2));
         dd = parseInt(day);
         yy = parseInt(year.slice(2, 4));
         mm = parseInt(month);
     
-        var akanDay = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+        let akanDay = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
     
         if (gender.toLowerCase() === "female"){
             return female[parseInt(akanDay) - 1];
@@ -19,18 +19,18 @@ $(Document).ready(function(){
         }
     }
 
-    $("form#form").submit(function(e){
+    $("form#form").submit((e) => {
         e.preventDefault();
 
-        var name = $("#name").val();
-        var date = $("#date").val();
+        let name = $("#name").val();
+        let date = $("#date").val();
         date = date.replace(/-/g, '');
-        var year = date.slice(0, 4);
-        var month = date.slice(4, 6);
-        var day = date.slice(6);
-        var gender = $("#select-gender").val();
+        let year = date.slice(0, 4);
+        let month = date.slice(4, 6);
+        let day = date.slice(6);
+        let gender = $("#select-gender").val();
 
-        var outputText = document.getElementById("output-text");
+        let outputText = document.getElementById("output-text");
         
         if(gender === "select-gender"){
             alert("No gender specified");
@@ -39,8 +39,7 @@ $(Document).ready(function(){
         } else if (date === ""){
             alert("date fields cannot be empty")
         } else {
-            // alert("Hi " + name + ", your akan name is: " + akanNameCalculator(day, month, year, gender));
-            outputText.innerHTML = "Hi " + name + ", your akan name is: " + akanNameCalculator(day, month, year, gender);
+            outputText.innerHTML = "Hi " + name + ", your akan name is: " + "<span id='akan-name'>"+ akanNameCalculator(day, month, year, gender) + "</span>";
         }
     });
 
